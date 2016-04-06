9e9bd08f63020a5e0c795f61314cb679161903d9
ean <eanplatter@gmail.com>
2016-04-05 21:29:08 -0500

    FORKED_COMMIT

--- index.js
+++ index.js
@@ -18,21 +18,31 @@ github.authenticate({
     token: process.env.FORKED_TOKEN,
 })
 
+// const meta = parser(shell.exec('cat package.json | json repository.url').stdout)
 
-const meta = parser(shell.exec('cat package.json | json repository.url').stdout)
+const pkg = shell.cat('package.json')
+let meta
+try {
+  meta = JSON.parse(pkg).repository.url
+} catch (e) {
+  throw new Error(e)
+}
 
-const index = meta.repo.indexOf('.git')
+
+const index = parser(meta).repo.indexOf('.git')
 if (index !== -1) {
   meta.repo = meta.repo.slice(0, index)
 }
 
-github.repos.fork({
-  user: meta.user,
-  repo: meta.repo,
-}, (err, res) => {
-  if (err) {
-    console.log('Hrmm, looks like something went wrong')
-  } else {
-    console.log('Hey it worked!')
-  }
-})
+// github.repos.fork({
+//   user: meta.user,
+//   repo: meta.repo,
+// }, (err, res) => {
+//   if (err) {
+//     console.log('Hrmm, looks like something went wrong')
+//   } else {
+//     console.log('Hey it worked!')
+//   }
+// })
+
+shell.exec(`git init && git add . && git commit -m "FORKED_COMMIT" && git push -u origin master`)
